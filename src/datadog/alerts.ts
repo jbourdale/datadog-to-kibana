81f4eaf2456dd8e46c9d73b168e178765028ac4d
Jules Bourdalé <jules.bourdale@sendinblue.com>
2022-08-19 16:30:30 +0200

    feat: adding updating monitors to add tags

--- src/datadog/alerts.ts
+++ src/datadog/alerts.ts
@@ -1,26 +1,125 @@
-import { v1 } from "@datadog/datadog-api-client"
-import { LOG_ALERT } from "@datadog/datadog-api-client/dist/packages/datadog-api-client-v1/models/MonitorType"
+import { v1, client } from "@datadog/datadog-api-client";
+import { LOG_ALERT } from "@datadog/datadog-api-client/dist/packages/datadog-api-client-v1/models/MonitorType";
 
-import axios from 'axios'
+const configuration = client.createConfiguration();
+const apiInstance = new v1.MonitorsApi(configuration);
 
-export const fetchMonitorsBasedOnLogs = async (): Promise<Array<v1.Monitor>> => {
-    console.log('dtk:datadog:alerts | Fetching monitors')
-    // Using axios instead of datadog client due to a bug inside listMonitors() that doesn't allow 
-    // to fetch monitors that I guess are too old
-    const response = await axios.get('https://api.datadoghq.com/api/v1/monitor', {
-        headers: {
-            'Content-Type': 'application/json',
-            'DD-API-KEY': process.env.DD_API_KEY || '',
-            'DD-APPLICATION-KEY': process.env.DD_APP_KEY || ''
-        }
-    })
-    console.log('dtk:datadog:alerts | Done')
+import axios from "axios";
 
-    const monitors = response.data as Array<v1.Monitor>
-    console.log('dtk:datadog:alerts | Monitors count found : ', monitors.length)
+export const fetchMonitorsBasedOnLogs = async (): Promise<
+  Array<v1.Monitor>
+> => {
+  console.log("dtk:datadog:alerts | Fetching monitors");
+  // Using axios instead of datadog client due to a bug inside listMonitors() that doesn't allow
+  // to fetch monitors that I guess are too old
+  const response = await axios.get("https://api.datadoghq.com/api/v1/monitor", {
+    headers: {
+      "Content-Type": "application/json",
+      "DD-API-KEY": process.env.DD_API_KEY || "",
+      "DD-APPLICATION-KEY": process.env.DD_APP_KEY || "",
+    },
+  });
+  console.log("dtk:datadog:alerts | Done");
 
-    const monitorsUsingLogs = monitors?.filter(m => m.type === LOG_ALERT)
-    
-    console.log('dtk:datadog:alerts | Monitors using logs count found', monitorsUsingLogs?.length)
-    return monitorsUsingLogs
-}
+  const monitors = response.data as Array<v1.Monitor>;
+  console.log("dtk:datadog:alerts | Monitors count found : ", monitors.length);
+
+  const monitorsUsingLogs = monitors?.filter((m) => m.type === LOG_ALERT);
+
+  console.log(
+    "dtk:datadog:alerts | Monitors using logs count found",
+    monitorsUsingLogs?.length
+  );
+  return monitorsUsingLogs;
+};
+
+export const fetchMonitorsUsingApmServiceWithoutTags = async (): Promise<
+  Array<v1.Monitor>
+> => {
+  console.log("dtk:datadog:alerts | Fetching monitors");
+
+  // Using axios instead of datadog client due to a bug inside listMonitors() that doesn't allow
+  // to fetch monitors that I guess are too old
+  const response = await axios.get("https://api.datadoghq.com/api/v1/monitor", {
+    headers: {
+      "Content-Type": "application/json",
+      "DD-API-KEY": process.env.DD_API_KEY || "",
+      "DD-APPLICATION-KEY": process.env.DD_APP_KEY || "",
+    },
+  });
+
+  console.log("dtk:datadog:alerts | Done");
+
+  const monitors = response.data as Array<v1.Monitor>;
+  console.log("dtk:datadog:alerts | Monitors count found : ", monitors.length);
+
+  const monitorsWithQueryIncludingService = monitors?.filter(
+    (m) => m.query.includes("service:") && m.type !== LOG_ALERT
+  );
+  console.log(
+    "dtk:datadog:alerts | Monitors using service in query : ",
+    monitorsWithQueryIncludingService.length
+  );
+
+  const serviceRg = new RegExp('("|{| )service:[a-z-._0-9]*', "g");
+  const monitorsAndTheirServices = monitorsWithQueryIncludingService?.reduce(
+    (acc, m) => {
+      const services = Array.from(
+        new Set(
+          m.query
+            .match(serviceRg)
+            ?.map((s) => s.substring(1)) // remove " or { matched to be sure that it's only service: and not kube_service: for instance
+            .filter((s) => s !== "service:") // remove services key that couldn't have been parsed (like queries with OR statements)
+        )
+      ); // remove duplicate using Set
+
+      acc.push({
+        services,
+        monitor: m,
+      });
+      return acc;
+    },
+    [] as Array<{ services: string[]; monitor: v1.Monitor }>
+  );
+
+  const monitorsWithoutTheirServicesTags = monitorsAndTheirServices.filter(
+    (ms) => {
+      const servicesNotInTags = ms.services.filter(
+        (s) => !ms.monitor.tags?.includes(s)
+      );
+      return servicesNotInTags.length > 0;
+    }
+  );
+
+  console.log(
+    "dtk:datadog:alerts | monitors without services tags count : ",
+    monitorsWithoutTheirServicesTags.length
+  );
+
+  const updatedMonitors = monitorsWithoutTheirServicesTags.map((ms) => {
+    ms.monitor.tags = [...(ms.monitor.tags || []), ...ms.services];
+    return ms.monitor;
+  });
+
+  console.log(
+    "dtk:datadog:alerts | updated monitors count : ",
+    updatedMonitors.length
+  );
+
+  updatedMonitors.forEach(async (m) => {
+    if (m.id) {
+      console.log("dtk:datadog:alerts | updating monitor : ", m.id);
+      try {
+        await apiInstance.updateMonitor({
+          monitorId: m.id,
+          body: m,
+        });
+        console.log("dtk:datadog:alerts | done : ", m.id);
+      } catch (e) {
+        console.error("ERROR OCCURED FOR MONITOR ", m.id, " : ", e);
+      }
+    }
+  });
+
+  return monitorsWithQueryIncludingService;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { fetchMonitorsBasedOnLogs } from "./datadog/alerts"
-import { fetchDashboardUsingLogs } from "./datadog/dashboards"
+import { fetchMonitorsUsingApmServiceWithoutTags } from "./datadog/alerts";
+// import { fetchDashboardUsingLogs } from "./datadog/dashboards";
 
 const run = async () => {
-    console.log('dtk:index | Start') // not using debug because it collides with datadog client library which is miss configured
-    
-    await fetchDashboardUsingLogs()
-    await fetchMonitorsBasedOnLogs()
+  console.log("dtk:index | Start"); // not using debug because it collides with datadog client library which is miss configured
 
-    console.log('dtk:index | Done')
-}
+  //   await fetchDashboardUsingLogs();
+  await fetchMonitorsUsingApmServiceWithoutTags();
 
-run()
+  console.log("dtk:index | Done");
+};
+
+run();
